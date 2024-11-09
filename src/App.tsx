4c82f49ed0eba70ea847f5fed528fcf8e52c412e
mcshell7 <mishakhamardiuk@gmail.com>
2024-11-09 11:38:35 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import FormComponent from "./components/FormComponent";
 
 function App() {
     return (
     <div className="App">
-        <FormComponent/>
     </div>
   );
 }
