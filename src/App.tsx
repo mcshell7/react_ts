e0f91c7222747761ff4fa5a6055809730a6dd94d
mcshell7 <mishakhamardiuk@gmail.com>
2024-11-16 11:05:23 -0500

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+    const [number, setNumber] = useState<number>(0);
+
+    useEffect(() => {
+    }, [number]);
+
     return (
     <div className="App">
+        <h2>{number}</h2>
+        <button onClick={() => {setNumber(prevState => prevState + 1);}}>increment</button>
+        <button onClick={() => {setNumber(prevState => prevState - 1)}}>decrement</button>
     </div>
   );
 }
